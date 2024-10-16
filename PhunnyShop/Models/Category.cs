743b63d124d5391dc1acc56921d945bec5caf5ec
Diogo Sal <diogosal1998@gmail.com>
2024-10-16 12:42:11 +0100

    - Adicionado pacotes EntityFrameworkCore -- EntityFrameworkCore -- EntityFrameworkCore.Tools -- EntityFrameworkCore.SqlServer
    - Criação de um Modelo Exemplo
    -- Models/Category.cs

---
+++ PhunnyShop/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ * Este modelo é apenas exemplo, não tem utilidade na Aplicação.
+ * Dentro da class, criamos os campos que desejamos ter na base de dados.
+ * Neste caso, temos 'Id', 'Name' e 'DisplayOrder'.
+*/
+
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PhunnyShop.Models
+{
+    public class Category
+    {
+        [Key]  // Data Annotation. Definimos que a propriedade abaixo será a Primary Key. Neste caso, 'Id'.
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
